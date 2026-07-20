63e2cd1797fb30369628614b51bd0295c52c71f9
Ashish Sharma <ashish.wrk06@gmail.com>
2026-07-20 20:52:52 +0530

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> st = new Stack<>();
+
+        for (String s : operations) {
+            if (s.equals("+")) {
+                int first = st.pop();
+                int second = st.peek();
+                st.push(first);              // restore
+                st.push(first + second);
+            } 
+            else if (s.equals("D")) {
+                st.push(st.peek() * 2);
+            } 
+            else if (s.equals("C")) {
+                st.pop();
+            } 
+            else {
+                st.push(Integer.parseInt(s));
+            }
+        }
+
+        int sum = 0;
+        while (!st.isEmpty()) {
+            sum += st.pop();
+        }
+
+        return sum;
+    }
+}
